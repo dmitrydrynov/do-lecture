2567eaa883f055ecb16edd1e08cf274cb2fcc1d8
Dmitry Drynov <dmitry.drynov@gmail.com>
2023-03-24 01:15:11 +0300

    Fix eslint bugs

--- apps/dapp/services/api.ts
+++ apps/dapp/services/api.ts
@@ -1,15 +1,1 @@
-export default class Api {
-	static getNetworkDetails = async (contractAddress: string) => {
-		const response = await fetch('/api/lecture/network-details', {
-			method: 'post',
-			body: JSON.stringify({ contractAddress }),
-			headers: {
-				accept: 'application/json',
-				'content-type': 'application/json',
-			},
-		})
-		const data = await response.json()
-
-		return data
-	}
-}
+export default class Api {}
